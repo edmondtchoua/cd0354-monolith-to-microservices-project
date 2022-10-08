e7dfe834bccaaaa43fd4f87d04587173ac6bb0d0
blog.tchoua@gmail.com <12345600>
2022-10-08 17:25:32 +0100

    Update frontend image

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a7f3cda5aa232458eb2a685074243c55-1707135202.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
