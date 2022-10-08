04b247851ab394e3f6214ddea276a111898ef44b
blog.tchoua@gmail.com <12345600>
2022-10-08 17:01:06 +0100

    update frontend image and setup ochestration with kubernate

---
+++ myscript.bat
@@ -0,0 +1,21 @@
+@echo off
+kubectl delete deploy backend-feed
+kubectl delete deploy backend-user
+kubectl delete deploy frontend
+kubectl delete deploy reverseproxy
+
+kubectl delete svc backend-feed
+kubectl delete svc backend-user
+kubectl delete svc frontend
+kubectl delete svc reverseproxy
+
+kubectl apply -f .\feed-service.yaml
+kubectl apply -f .\user-service.yaml
+kubectl apply -f .\frontend-service.yaml
+kubectl apply -f .\reverseproxy-service.yaml
+
+kubectl apply -f .\feed-deployment.yaml
+kubectl apply -f .\user-deployment.yaml
+kubectl apply -f .\frontend-deployment.yaml
+kubectl apply -f .\reverseproxy-deployment.yaml
+pause
